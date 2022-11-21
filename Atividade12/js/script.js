d0559b45cd67f624b002ae9f82c4144c74829227
Mateus Miranda <88382721+MateusJunio016@users.noreply.github.com>
2022-11-21 11:39:11 -0300

    Add files via upload

---
+++ Atividade12/js/script.js
@@ -0,0 +1,14 @@
+var janela = document.getElementById("janela");
+
+
+function abrirJanela(){
+        janela.src = "images/janelaaberta.png";
+}
+
+function fecharJanela(){
+        janela.src = "images/janelafechada.png";
+}
+
+function quebrarJanela(){
+        janela.src = "images/janelaquebra.png";
+}
